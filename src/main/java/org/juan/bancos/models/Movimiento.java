550d23338af3865ea586d0f56c4bb942f717d92b
Juan Hernandez <juan.martinez3168@gmail.com>
2025-02-22 18:58:58 -0600

    Modelos para saldo y movimiento

---
+++ src/main/java/org/juan/bancos/models/Movimiento.java
@@ -0,0 +1,17 @@
+package org.juan.bancos.models;
+
+
+import java.math.BigDecimal;
+
+public class Movimiento {
+
+    public Integer cuentaBancoId;
+
+    public String banco;
+
+    public BigDecimal depositos;
+
+    public BigDecimal retiros;
+
+
+}
---
+++ src/main/java/org/juan/bancos/models/Saldo.java
@@ -0,0 +1,18 @@
+package org.juan.bancos.models;
+
+import java.math.BigDecimal;
+
+public class Saldo {
+
+    public Integer cuentaBancoId;
+
+    public String banco;
+
+    public BigDecimal saldoInicial;
+
+    public BigDecimal depositors;
+
+    public BigDecimal retiros;
+
+    public BigDecimal SaldoFinal;
+}

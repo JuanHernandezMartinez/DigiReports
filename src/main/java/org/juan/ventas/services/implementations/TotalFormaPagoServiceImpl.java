363a4e76884c40e832a25136a6df77c4584f1076
Juan Hernandez <juan.martinez3168@gmail.com>
2025-03-10 16:32:54 -0600

    Logica sin uso

--- src/main/java/org/juan/ventas/services/implementations/TotalFormaPagoServiceImpl.java
+++ src/main/java/org/juan/ventas/services/implementations/TotalFormaPagoServiceImpl.java
@@ -1,13 +1,10 @@
 package org.juan.ventas.services.implementations;
 
-import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.juan.ventas.dtos.TotalFormaPago;
 import org.juan.ventas.repositories.TotalFormaPagoRepository;
-import org.juan.ventas.services.CompraService;
 import org.juan.ventas.services.TotalFormaPagoService;
-
 import java.time.LocalDate;
 import java.util.List;
 
